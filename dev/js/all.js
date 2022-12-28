f4be851641156a09659957476167151197635dfa
kate <katyavov2805@gmail.com>
2022-12-28 12:37:21 +0200

    final

---
+++ dev/js/all.js
@@ -0,0 +1,104 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let popupBg = document.querySelector('.popup__bg');
+    let popup = document.querySelector('.popup');
+    let openPopupButtons = document.querySelectorAll('.ref__btn');
+    let closePopupButton = document.querySelector('.close-popup');
+    
+    openPopupButtons.forEach((button) => {
+        button.addEventListener('click', (e) => {
+            e.preventDefault();
+            popupBg.classList.add('active');
+            popup.classList.add('active');
+        })
+    });
+    
+    closePopupButton.addEventListener('click',() => {
+        popupBg.classList.remove('active');
+        popup.classList.remove('active');
+    });
+    
+    document.addEventListener('click', (e) => {
+        if(e.target === popupBg) {
+            popupBg.classList.remove('active');
+            popup.classList.remove('active');
+        }
+    });
+});
+document.addEventListener("DOMContentLoaded", () => {
+});
+var accordeonButtons = document.getElementsByClassName("accordeon__button");
+
+//пишем событие при клике на кнопки - вызов функции toggle
+for(var i = 0; i < accordeonButtons.length; i++) {
+    var accordeonButton = accordeonButtons[i];
+
+    accordeonButton.addEventListener("click", toggleItems, false);
+}
+
+//пишем функцию
+function toggleItems() {
+
+    // переменная кнопки(актульная) с классом
+    var itemClass = this.className;
+
+    // добавляем всем кнопкам класс close
+    for(var i = 0; i < accordeonButtons.length; i++) {
+        accordeonButtons[i].className = "accordeon__button closed";
+    }
+
+    // закрываем все открытые панели с текстом
+    var pannels = document.getElementsByClassName("accordeon__panel");
+    for (var z = 0; z < pannels.length; z++) {
+        pannels[z].style.maxHeight = 0;
+    }
+
+    // проверка. если кнопка имеет класс close при нажатии
+    // к актуальной(нажатой) кнопке добававляем активный класс
+    // а панели - которая находится рядом задаем высоту
+    if(itemClass == "accordeon__button closed") {
+        this.className = "accordeon__button active";
+        var panel = this.nextElementSibling;
+        panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+
+}
+function getTimeRemaining(endtime) {
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    return {
+      'total': t,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
+  
+  function initializeClock(id, endtime) {
+    var clock = document.getElementById(id);
+    var minutesSpan = clock.querySelector('.minutes');
+    var secondsSpan = clock.querySelector('.seconds');
+  
+    function updateClock() {
+      var t = getTimeRemaining(endtime);
+  
+      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+  
+      if (t.total <= 0) {
+        clearInterval(timeinterval);
+      }
+    }
+  
+    updateClock();
+    var timeinterval = setInterval(updateClock, 1000);
+  }
+  
+  var deadline = "January 01 2023 00:00:00 GMT+0300"; //for Ukraine
+  var deadline = new Date(Date.parse(new Date()) + 1 * 1 * 31 * 29 * 1000); // for endless timer
+  initializeClock('countdown', deadline);
+  $('.my').change(function () {
+    if ($(this).val() != '') $(this).prev().text('Выбрано файлов: ' + $(this)[0].files.length);
+    else $(this).prev().text('Выберите файлы');
+  });
